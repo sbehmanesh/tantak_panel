7a2304a01f9413461f74f32f4333ebfc7f7f69c1
mojtabaie.m76 <mojtabaiie.m76@gmail.com>
2024-09-04 19:37:25 +0330

    طراحی داشبورد

---
+++ components/dashboard/SideCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    width="110"
+    height="170"
+    class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
+  >
+    <v-icon>{{ card.icon }}</v-icon>
+    <div class="my-3 text-center">{{ card.title }}</div>
+    <span>{{ card.count }}</span>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+    },
+  },
+  data: () => ({}),
+};
+</script>
--- pages/panel/index.vue
+++ pages/panel/index.vue
@@ -13,45 +13,14 @@
           <v-divider color="#fd5800" class="mt-3"></v-divider>
         </v-card>
       </v-col>
-      <v-col cols="3">
-        <v-card
-          width="110"
-          height="170"
-          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
-        >
-          <v-icon>fact_check</v-icon>
-          <div class="my-3 text-center">کارهای امروز من</div>
-          <span>0</span>
-        </v-card>
-        <v-card
-          width="110"
-          height="170"
-          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
-        >
-          <v-icon>fact_check</v-icon>
-          <div class="my-3 text-center">کارهای دارای تاخیر</div>
-          <span>0</span>
-        </v-card>
-        <v-card
-          width="110"
-          height="170"
-          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
-        >
-          <v-icon>fact_check</v-icon>
-          <div class="my-3 text-center">کارهای قابل پیگیری</div>
-          <span>0</span>
-        </v-card>
-        <v-card
-          width="110"
-          height="170"
-          class="d-flex flex-column justify-center align-center pa-3 my-2 green-border border-radius-10 elevation-0"
-        >
-          <v-icon>fact_check</v-icon>
-          <div class="my-3 text-center">پیام های شخصی من</div>
-          <span>6</span>
-        </v-card>
+      <v-col
+        cols="12"
+        md="3"
+        :class="$vuetify.breakpoint.mdAndUp ? '' : 'd-flex'"
+      >
+        <SideCard v-for="(card, i) in sides" :key="i" :card="card" />
       </v-col>
-      <v-col cols="6" style="">
+      <v-col cols="12" md="6">
         <v-sheet class="d-flex align-center gray">
           <div>همکاران من:</div>
           <div class="mr-3">
@@ -65,12 +34,36 @@
   </v-container>
 </template>
 <script>
-import MainTable from '@/components/dashboard/MainCard'
+import MainTable from "@/components/dashboard/MainCard";
+import SideCard from "@/components/dashboard/SideCard";
 export default {
-components: {MainTable},
+  components: { MainTable, SideCard },
   data: () => ({
     title: "کارتابل",
     roles: [],
+
+    sides: [
+      {
+        icon: "fact_check",
+        title: "کارهای امروز من",
+        count: "",
+      },
+      {
+        icon: "fact_check",
+        title: "کارهای دارای تاخیر",
+        count: "",
+      },
+      {
+        icon: "fact_check",
+        title: "کارهای قابل پیگیری",
+        count: "",
+      },
+      {
+        icon: "fact_check",
+        title: "پیام های شخصی من",
+        count: "",
+      },
+    ],
   }),
   computed: {
     person() {
